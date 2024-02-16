1fa542cac714fbcd9cd62dbe9700ecae708ca86c
gongdaoyuito <83847483+gongdaoyuito@users.noreply.github.com>
2024-02-16 13:08:43 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  typescript: {
+    // !! 警告 !!
+    // あなたのプロジェクトに型エラーがあったとしても、プロダクションビルドを正常に完了するために危険な許可をする。
+    // !! 警告 !!
+    ignoreBuildErrors: true
+  }
+};
